2606971dccf12556985589fc8881a43e4b6fbc25
P. Envall <petter.envall@gmail.com>
2021-01-12 23:06:33 +0100

    handle cookie options

--- frameworks/oak.ts
+++ frameworks/oak.ts
@@ -1,15 +1,29 @@
+import { Cookie } from "https://deno.land/std/http/cookie.ts";
 import { SessionData } from "../mod.ts";
 
-export default function use(session: any) {
+type CookieOptions = Omit<Cookie, "value" | "name">;
+
+export default function use(session: any, options: CookieOptions = {}) {
 	return async (context: any, next: any) => {
-		const sid = context.cookies.get("sid");
+        const sid = context.cookies.get("sid");
+        // set default cookie options
+        const { protocol } = context.request.url;
+        if (!options.secure) {
+            options.secure = "http:" === protocol;
+		}
+		if (!options.path) {
+			options.path = "/"
+		}
+        if (!options.httpOnly) {
+            options.httpOnly = "http:" === protocol;
+		}
 		
 		if (sid === undefined) {
 			context.state.session = new SessionData(session);
-			context.cookies.set("sid", context.state.session.sessionId);
+			context.cookies.set("sid", context.state.session.sessionId, options);
 		} else if(session._store.sessionExists(sid) === false) {
 			context.state.session = new SessionData(session);
-			context.cookies.set("sid", context.state.session.sessionId);
+			context.cookies.set("sid", context.state.session.sessionId, options);
 		} else {
 			context.state.session = new SessionData(session, sid);
 		}
